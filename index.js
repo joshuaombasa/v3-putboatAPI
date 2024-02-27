1ea732a26f435a952eac680defe2e9de15f45e11
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-27 10:42:54 +0300

    add logic to start and manage server app

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+
+app.listen(config.PORT,() => {
+    logger.info(`server running on port ${config.PORT}`)
+})
